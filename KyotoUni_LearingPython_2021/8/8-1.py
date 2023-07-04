c7c3968c63c98f317aab841bdefa839120b81a28
Yuuki Takagi <snow7777man@gmail.com>
2023-07-04 09:15:33 +0900

    add new files

--- KyotoUni_LearingPython_2021/8/8-1.py
+++ KyotoUni_LearingPython_2021/8/8-1.py
@@ -1,8 +1,8 @@
-import turtle
+from turtle import *
 
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
 
-turtle.done()
+done()
